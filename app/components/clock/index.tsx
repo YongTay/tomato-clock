f267ac1fc7cf89730d6b1891945b765a65619d1c
yongtay <572194404@qq.com>
2024-09-20 09:50:01 +0800

    feat: 增加禁止熄屏

--- app/components/clock/index.tsx
+++ app/components/clock/index.tsx
@@ -6,8 +6,25 @@ function fixNumber(n: number) {
   return n < 10 ? '0' + n : '' + n
 }
 
+async function requestWakeLock(fn) {
+  try {
+    const wakeLock = await navigator.wakeLock.request('screen');
+    console.log('屏幕已禁止熄屏');
+    fn()
+  } catch (err) {
+    console.error('无法禁止屏幕熄屏：', err);
+  }
+}
+
 export default function Clock() {
   const [date, setDate] = React.useState(new Date())
+  const [wakeLock, setWakeLock] = React.useState(false)
+  React.useEffect(() => {
+    if (wakeLock) return
+    requestWakeLock(() => {
+      setWakeLock(true)
+    })
+  }, [wakeLock])
   React.useEffect(() => {
     setTimeout(() => {
       setDate(new Date())
